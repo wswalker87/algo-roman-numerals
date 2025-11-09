b3eeeb97fdb5eab848a7948cd3ed202aa3918281
wswalker87 <scott@wscottwalker.com>
2025-11-09 12:51:32 -0600

    final

--- romanNumerals.js
+++ romanNumerals.js
@@ -15,14 +15,13 @@ function toRomanLazy(num) { // 6
   // variable that has an array with numerals in descending order
   let romanNumeralPriorityOrder = ['M', 'D', 'C', 'L', 'X', 'V', 'I']
   // iterate over romanNumeralPriorityOrder
-  let romanNumArray = []
   for (let letter of romanNumeralPriorityOrder) {
     const arabicNum = romanNumeralToArabic[letter];
-    console.log(arabicNum)
+    // console.log(arabicNum)
     let numberOfRepeats = (Math.floor(num / romanNumeralToArabic[letter]))
-    console.log(`The number of repeats is ${numberOfRepeats}`)
+    // console.log(`The number of repeats is ${numberOfRepeats}`)
 
-    for(let i = 0; i < numberOfRepeats; i++){
+    for(let i = 0; i < numberOfRepeats; i++){ // init i to 0, if i is less than numOfRepeats>> output letter and subtract the arabic equiv from num, increment the i
         output = output + letter
         num = num - arabicNum
     }
@@ -37,14 +36,45 @@ function toRomanLazy(num) { // 6
 
 
 function toRoman(num) {
-  
-  
-  
-  
-  
-  
-  
-    return "";
+  // init a variable output to an empty string
+  let output = ""
+
+  // create an object that that maps keys in roman numerals and the value to the arabic numeral //Variable name is romanNumeralToArabic
+  let romanNumeralToArabic = {
+    I: 1,
+    IV: 4,
+    V: 5,
+    IX: 9,
+    X: 10,
+    XL: 40,
+    L: 50,
+    C: 100,
+    CD: 400,
+    D: 500,
+    CM: 900,
+    M: 1000,
+  }
+  // variable that has an array with numerals in descending order
+  let romanNumeralPriorityOrder = ['M', 'CM', 'D', 'CD', 'C', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+  // iterate over romanNumeralPriorityOrder
+  for (let letter of romanNumeralPriorityOrder) {
+    const arabicNum = romanNumeralToArabic[letter];
+    // console.log(arabicNum)
+    let numberOfRepeats = (Math.floor(num / romanNumeralToArabic[letter]))
+    // console.log(`The number of repeats is ${numberOfRepeats}`)
+
+    for(let i = 0; i < numberOfRepeats; i++){
+        output = output + letter
+        num = num - arabicNum
+    }
+  }
+
+  return output;
+
 }
+
+// console.log(toRomanLazy(4)); // 'IIII'
+// console.log(toRomanLazy(150)); // 'CL'
+// console.log(toRomanLazy(944)); // 'DCCCCXXXXIIII'
 
 module.exports = { toRoman, toRomanLazy };
